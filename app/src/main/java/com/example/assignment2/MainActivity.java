d1858fa2ec35723c5c3984a42234d868c586a20f
Kyle Ehlers <kce0002@auburn.edu>
2019-02-26 10:32:42 -0600

    Created ability to add notes to an arraylist variable

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.assignment2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button addNoteButton;
+    EditText editText;
+    List<String> notes = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addNoteButton = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+    }
+
+    public void addNote(View v) {
+        String note = editText.getText().toString().trim();
+        if (!note.equals("")) {
+            notes.add(note);
+        }
+        System.out.println(notes.toString());
     }
 }
